97086684ba8d06e7ddcc57e20d99fbced1b4d0e6
Amanda <amandajuhlsilberg@hotmail.com>
2025-04-08 23:59:14 +0200

    DateTime practise

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace HejDateTime
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime date1 = DateTime.Now;
+            Console.WriteLine(date1.ToString());
+
+            // Get date-only portion of date, without its time.
+            DateTime dateOnly = date1.Date;
+            // Display date using short date string.
+            Console.WriteLine(dateOnly.ToString("d"));
+            // Display date using 24-hour clock.
+            Console.WriteLine(dateOnly.ToString("g"));
+            Console.WriteLine(dateOnly.ToString("MM/dd/yyyy HH:mm"));
+
+            Console.ReadLine();
+        }
+    }
+}
