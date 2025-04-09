5d4c80a2da642853748c90b311cf6b7f95fa723d
Amanda <amandajuhlsilberg@hotmail.com>
2025-04-09 14:53:16 +0200

    made a test for saving files.

---
+++ TestingFiles/TestingFiles/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+namespace TestingFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string writeText = "Det her er en gemt tekst";
+            File.WriteAllText("gemtTekst.txt", writeText);
+
+            string readText = File.ReadAllText("gemtTekst.txt");
+            Console.WriteLine(readText);
+            Console.ReadLine();
+        }
+    }
+}
