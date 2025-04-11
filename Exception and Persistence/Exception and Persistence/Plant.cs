d14e22ede19477b39e01eb53d97056f58abcaedf
Amanda <amandajuhlsilberg@hotmail.com>
2025-04-11 10:42:59 +0200

    Made new ConsoleApp and made a class in it.

---
+++ Exception and Persistence/Exception and Persistence/Plant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace Exception_and_Persistence
+{
+    public class Plant
+    {
+        // Properties - because they have getters and setter, otherwise they'd be fields.
+        string _type { get; set; }
+        string _name { get; set; }
+        string _image { get; set; }
+
+        //Constructor time
+        public Plant(string type, string name, string image)
+        {
+            _type = type;
+            _name = name;
+            _image = image;
+        }
+
+        string top = "_ * 35";
+        // Method to display plant info
+        public void DisplayPlantInfo()
+        {
+            Console.WriteLine($"{top}");
+        }
+
+    }
+}
---
+++ Exception and Persistence/Exception and Persistence/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exception_and_Persistence
+{
+    internal class Program
+    {
+        public void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello, World!");
+
+            plant.DisplayPlantInfo();
+        }
+    }
+}
