09ee7d3240cd8292d5adf1fc2e9de9233c74c384
Amanda <amandajuhlsilberg@hotmail.com>
2025-04-28 14:26:10 +0200

    Started a WPF file to test MVVM

---
+++ WpfTestApp/WpfTestApp/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfTestApp.Model;
+
+namespace WpfTestApp.ViewModel
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+       public ObservableCollection<Item> Items { get; set; }
+
+        public MainWindowViewModel() 
+        {
+        
+        }
+
+        private Item _selectedItem;
+
+        
+
+        public Item SelectedItem
+        {
+            get {  return _selectedItem; }
+            set { _selectedItem = value; }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void 
+    }
+}
