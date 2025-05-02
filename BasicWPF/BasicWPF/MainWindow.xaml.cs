6b6432790c10b8cf71e2a313be4a3e9c5e7729ec
Amanda <amandajuhlsilberg@hotmail.com>
2025-05-02 23:51:34 +0200

    Added: Background image, "Add Png" Button and "Add Gif" Button

--- BasicWPF/BasicWPF/MainWindow.xaml.cs
+++ BasicWPF/BasicWPF/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace BasicWPF
 {
@@ -19,26 +20,52 @@ namespace BasicWPF
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private string boundText;
+        public string BoundText
+        {
+            get { return boundText; }
+            set
+            {
+                boundText = value;
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText")); - Old Method.
+                OnPropertyChanged(); // We don'r have to write ("Bound Text") because the method uses [CallerMemberName]
+            }
+        }
+
+        private ImageSource _boundImage;
+
+        public ImageSource BoundImage
+        {
+            get { return _boundImage; }
+            set 
+            { 
+                _boundImage = value; 
+                OnPropertyChanged();
+            }
+        }
+        private ImageSource _boundGif;
+
+        public ImageSource BoundGif
+        {
+            get { return _boundGif; }
+            set
+            {
+                _boundGif = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainWindow()
         {
             DataContext = this; // would be a ViewModel in MVVM
             InitializeComponent();
         }
 
-        private string boundText;
+       
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        public string BoundText
-        {
-            get { return boundText; }
-            set 
-            { 
-                boundText = value;
-                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText")); - Old Method.
-                OnPropertyChanged(); // We don'r have to write ("Bound Text") because the method uses [CallerMemberName]
-            }
-        }
+       
         private void btnSet_Click(object sender, RoutedEventArgs e)
         {
 
@@ -57,7 +84,7 @@ namespace BasicWPF
         {
 
             OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.Filter = "C# Source Files | *.cs"; //This filters is so it shows ANY files with .cs at the end.
+            fileDialog.Filter = "C# files| *.cs"; //This filters is so it shows ANY files with .cs at the end.
             fileDialog.InitialDirectory = "C:\\Users\\Amanda\\source\\repos"; //chooses what folder we start in.
             fileDialog.Title = "Nu SKAL du vælge .cs fil(er)! :)";
             //fileDialog.Multiselect = true;
@@ -76,7 +103,52 @@ namespace BasicWPF
             }
         }
 
-        
+        private void btnOpenImage_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Png  | *.png"; //This filters is so it shows ANY files with .cs at the end.
+            fileDialog.InitialDirectory = "E:\\Downloads"; //chooses what folder we start in.
+            fileDialog.Title = "Nu SKAL du vælge en .png fil! :)";
+            //fileDialog.Multiselect = true;
+
+            bool? success = fileDialog.ShowDialog();
+            if (success == true)
+
+            {
+               BoundImage = new BitmapImage(new Uri(fileDialog.FileName));
+            }
+            else
+            {
+                //didn't pick anything!
+            }
+        }
+        private void btnOpenGif_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "GIF FILES(*.gif) | *.gif"; //This filters is so it shows ANY files with .cs at the end.
+            fileDialog.InitialDirectory = "E:\\Downloads"; //chooses what folder we start in.
+            fileDialog.Title = "Nu SKAL du vælge en .gif fil! :)";
+            //fileDialog.Multiselect = true;
+
+            bool? success = fileDialog.ShowDialog();
+            if (success == true)
+
+            {
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.UriSource = new Uri(fileDialog.FileName);
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.EndInit();
+
+                WpfAnimatedGif.ImageBehavior.SetAnimatedSource(GifImageControl, image);
+            }
+            else
+            {
+                //didn't pick anything!
+            }
+        }
 
         private void OnPropertyChanged([CallerMemberName]string propertyName = null)
         {
