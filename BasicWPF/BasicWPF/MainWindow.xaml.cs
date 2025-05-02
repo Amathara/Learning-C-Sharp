dd5f5a9b8397f987ed2a04e2d08a851b6aa1c1bb
Amanda <amandajuhlsilberg@hotmail.com>
2025-05-02 15:15:04 +0200

    Bound a button to some code. Next I'm gonna try a message box.

---
+++ BasicWPF/BasicWPF/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BasicWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this; // would be a ViewModel in MVVM
+            InitializeComponent();
+        }
+
+        private string boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set 
+            { 
+                boundText = value;
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText")); - Old Method.
+                OnPropertyChanged(); // We don'r have to write ("Bound Text") because the method uses [CallerMemberName]
+            }
+        }
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Set From Code";
+            MessageBox.Show("Your Message here!");
+        }
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
