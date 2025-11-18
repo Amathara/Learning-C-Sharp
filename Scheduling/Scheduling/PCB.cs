dca7966fac989df679fd9c0095ea1c248cd1771a
Amanda <amandajuhlsilberg@hotmail.com>
2025-11-18 17:59:58 +0100

    stuff

---
+++ Scheduling/Scheduling/PCB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Scheduling
+{
+    public enum ProcessStateType {New, Ready, Run, Exit, Wait};
+    internal class PCB
+    {
+        public string ProcessName { get; set; }
+        public int ProcessPriority { get; set; }
+        public ProcessStateType ProcessState { get; set; }
+
+      
+        
+public override string ToString()
+        {
+            return $"{ProcessName},({ProcessPriority})";
+        }
+
+       
+    }
+}
---
+++ Scheduling/Scheduling/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading;
+namespace Scheduling
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(MethodThread);
+            thread.Start("Hello, Method!");
+
+            Thread thread2 = new Thread((object message) => {
+                string text = (string)message;
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.WriteLine(text);
+                    Thread.Sleep(100);
+                }
+            });
+            thread2.Start("Hello, Lambda!");
+            Thread thread3 = new Thread(delegate (object message) {
+                string text = (string)message;
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.WriteLine(text);
+                    Thread.Sleep(100);
+                }
+            });
+            thread3.Start("Hello, Anon!");
+
+
+            Console.ReadLine();
+
+        }
+
+        static void MethodThread(object message)
+        {
+            string text = (string)message;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(text);
+                Thread.Sleep(100);
+            }
+        }
+
+      
+        
+    }
+}
